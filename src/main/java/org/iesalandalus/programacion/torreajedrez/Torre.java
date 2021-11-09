97f43ae59e9b9f13b504e160d551a3136a85eb82
Alsabar92 <43401436+Alsabar92@users.noreply.github.com>
2021-11-10 00:06:04 +0100

    Creamos getters y setters de los atributos

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -4,4 +4,26 @@ public class Torre {
 	// Atributos
 	private Color color;
 	private Posicion posicion;
+	
+	
+	// Creamos setters y getters
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setColor(Color color) {
+		if (color == null)
+		{
+			throw new NullPointerException("ERROR: Columna no v�lida.");
+		}
+		this.color = color;
+	}
+	
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	
+	public void setPosicion(Posicion posicion) { //DUDA
+		this.posicion=posicion;
+	}
 }
