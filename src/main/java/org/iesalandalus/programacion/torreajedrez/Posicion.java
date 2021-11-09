9ca163713ef48e9c1e42bac7b8e40420c7ba1cec
Alsabar92 <43401436+Alsabar92@users.noreply.github.com>
2021-11-09 23:42:27 +0100

    Añado los setters y getters en la clase posición

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -3,4 +3,66 @@ package org.iesalandalus.programacion.torreajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
+	
+	private final int MIN_FILA=1;
+	private final int MAX_FILA=8;
+	
+	
+	//Getters y Setters
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila < MIN_FILA || fila > MAX_FILA ) 
+		{
+			throw new IllegalArgumentException("ERROR: Fila no válida");
+		} else 
+		{
+			this.fila = fila;
+		}
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		if (columna!='a' && columna!='b' && columna!='c' && columna!='d' && columna!='e' && columna!='f' && columna!='g' && columna!='h' && columna!='A' && columna!='B' && columna!='C' && columna!='D' && columna!='E' && columna!='F' && columna!='G' && columna!='H')
+		{
+			throw new IllegalArgumentException("ERROR: La columna no es correcta");
+		}
+		else {
+			// Comprobamos si se ha introducido la columna en mayúscula y si es así, la convertimos a minúscula. La opción default mantiene la solución, dado que tenemos validada la entrada desde el if.
+			switch (columna) {
+				case 'A':
+					this.columna='a';
+					break;
+				case 'B':
+					this.columna='b';
+					break;
+				case 'C':
+					this.columna='c';
+					break;
+				case 'D':
+					this.columna='d';
+					break;
+				case 'E':
+					this.columna='e';
+					break;
+				case 'F':
+					this.columna='f';
+					break;
+				case 'G':
+					this.columna='g';
+					break;
+				case 'H':
+					this.columna='h';
+					break;
+				default:
+					this.columna = columna;
+					break;
+			}
+		}	
+	}
+	
+	
+	
 }
