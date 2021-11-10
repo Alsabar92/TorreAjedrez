9a4d836bb374952831c300034862b4903c606d68
Alsabar92 <43401436+Alsabar92@users.noreply.github.com>
2021-11-10 18:27:51 +0100

    Creo el método mostrarMenu con las opciones disponibles.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -9,7 +9,7 @@ public class MainApp {
 	}
 
 	
-	//Métodos
+	//Métodos. Deben declararse privados para que no se tenga acceso desde las clases externas.
 	//Metodo mostrarTorre
 	private static void mostrarTorre() {
 		try {
@@ -17,5 +17,19 @@ public class MainApp {
 		} catch (NullPointerException e){
 			System.out.println("ERROR: La torre no está creada.");
 		}
+	}
+	
+	//Método mostrarMenu
+	private static void mostrarMenu() {
+		System.out.println("=====================================");
+		System.out.println("Programa simulador. Torre de Ajedrez.");
+		System.out.println("=====================================");
+		System.out.println(" ");
+		System.out.println("Por favor, elija una opción a realizar:");
+		System.out.println("1. Crear una torre por defecto.");
+		System.out.println("2. Crear una torre eligiendo su color.");
+		System.out.println("3. Crear una torre eligiendo su color y posición inicial.");
+		System.out.println("4. Mover la torre.");
+		System.out.println("5. Salir.");
 	}
 }
