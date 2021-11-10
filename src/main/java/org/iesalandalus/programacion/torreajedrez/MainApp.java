9fba7d77f8366872528e80c6227db044a96b3898
Alsabar92 <43401436+Alsabar92@users.noreply.github.com>
2021-11-10 18:44:40 +0100

    Creo el método elegirOpcion y valido el valor introducido por teclado.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.torreajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class MainApp {
 	private static Torre torre;
 	
@@ -25,11 +27,20 @@ public class MainApp {
 		System.out.println("Programa simulador. Torre de Ajedrez.");
 		System.out.println("=====================================");
 		System.out.println(" ");
-		System.out.println("Por favor, elija una opción a realizar:");
 		System.out.println("1. Crear una torre por defecto.");
 		System.out.println("2. Crear una torre eligiendo su color.");
 		System.out.println("3. Crear una torre eligiendo su color y posición inicial.");
 		System.out.println("4. Mover la torre.");
 		System.out.println("5. Salir.");
+	}
+	
+	//Método elegirOpcion
+	private static int elegirOpcion() {
+		int opcionMenu;
+		System.out.print("Por favor, elija una de las anteriores opciones: ");
+		do {
+			opcionMenu = Entrada.entero();
+		} while (opcionMenu<0 || opcionMenu>5);
+		return opcionMenu;	
 	}
 }
